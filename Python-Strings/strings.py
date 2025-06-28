e126fd109020373816af57d3eaf66105e33f09ca
CoderCat010 <nusraumme@gmail.com>
2025-06-28 09:31:26 +0600

    added practice no: 1

---
+++ Python-Strings/strings.py
@@ -0,0 +1,16 @@
+# index number
+'''x = 'hello'
+print(x[3])
+'''
+
+# length
+'''y = 'HEY!'
+print(len(y))'''
+
+
+# Slicing
+names = 'MEGH'
+
+'''print(names[0:3])
+print(names[:3])
+print(names[2:])'''
